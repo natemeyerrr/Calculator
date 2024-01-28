5ec7e4cf0a5859bdcec7aa6a71097021c229052b
Nate Meyer <06najay@gmail.com>
2024-01-28 10:21:51 -0800

    made some changes to make it easier to use. Added a option to go back after adding or removing from the list.

--- to_do.py
+++ to_do.py
@@ -1,25 +1,40 @@
 to_do = []
 
-def to_do_list():
-    choosing = input("Would you like to add or remove from the list? ")
+print("Hello, Welcome to your To-Do List")
 
-    if choosing.lower() == "add":
-        while True:
-            add = input("Enter to add to your to-do list:\n(Enter 'no' to end) ")
-            if add.lower() == "no":
+def to_do_list():
+    while True:
+        choosing = input("Would you like to add, remove, or exit from the list? ")
+        
+        if choosing.lower() == "add":
+            while True:
+                print("Your to-do list:", to_do)
+                add = input("Enter to add to your to-do list (or enter 'no' to stop adding): ")
+                if add.lower() == "no":
+                    print("Your to-do list:", to_do)
+                    break
+                else:
+                    to_do.append(add)
+        elif choosing.lower() == "remove":
+            print("Your to-do list:", to_do)
+            where_to_remove = input("Enter the number of the item to remove (or enter 'no' to stop removing): ")
+            if where_to_remove.lower() == "no":
+                print("Your to-do list:", to_do)
                 break
+            elif where_to_remove.isdigit():
+                where_to_remove = int(where_to_remove)
+                if 1 <= where_to_remove <= len(to_do):
+                    removed_item = to_do.pop(where_to_remove - 1)
+                    print(f"Removed: {removed_item}")
+                else:
+                    print("Not a spot on the list. Enter a new number")
             else:
-                to_do.append(add)
-    elif choosing.lower() == "remove":
-        print("Your to-do list:", to_do)
-        where_to_remove = int(input("Enter the number of the item to remove: "))
-        if 1 <= where_to_remove <= len(to_do):
-            removed_item = to_do.pop(where_to_remove - 1)
-            print(f"Removed: {removed_item}")
+                print("Invalid input. Please enter a valid number.")
+                continue  # Continue the outer loop to ask for input again
+        elif choosing.lower() == 'exit':
+            break
         else:
-            print("Not a spot on the lsit. Enter a new number")
-    else:
-        print("Please enter 'add' or 'remove'.")
+            print("\n\n\nInvalid input. Please enter 'add', 'remove', or 'exit'.\n\n\n")
 
 to_do_list()
 print("Your to-do list:", to_do)
